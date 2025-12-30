57def1fc5e048473e74af0605868db8e6876bdc9
Charlie Van Belle <r0995521@student.thomasmore.be>
2025-12-30 22:34:08 +0100

    insert test carousel

--- script.js
+++ script.js
@@ -86,6 +86,25 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(changePhoto, 10000);
 });
 
+//Carousel
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+        if(newIndex < 0) newIndex = slides.children.length - 1;
+        if(newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    });
+});
+
 //LinkedIn icon hover
 let linkedinIcon = document.querySelector('.linkedin_icon');
 linkedinIcon.addEventListener('mouseover', function() {
